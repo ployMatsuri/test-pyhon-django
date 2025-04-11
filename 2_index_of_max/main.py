f61c9172550b288dd6dbac5769808a092187dd1d
ployMatsuri <ployvimonsirithammada@gmail.com>
2025-04-11 16:40:55 +0700

    solution 2_index_of_max

--- 2_index_of_max/main.py
+++ 2_index_of_max/main.py
@@ -20,4 +20,37 @@ output = list can not blank
 class Solution:
 
     def find_max_index(self, numbers: list) -> int | str:
-        pass
+        if not numbers:
+            return "list can not blank"
+        i=0
+        max_index = 0
+
+        for i in range(len(numbers)):
+            if numbers[i]<0:
+                return "number can not be negative"
+            
+            if numbers[i] >= numbers[max_index]:
+                max_index=i
+                
+        return max_index
+    
+try:
+    user_input = input("input = ")
+    
+    if not (user_input.startswith('[') and user_input.endswith(']')):
+        raise ValueError("Invalid format")
+
+    content = user_input.strip('[]').strip()
+    
+    if content == "":
+        numbers = []
+    else:
+        numbers = list(map(int, content.split(',')))
+
+    solution = Solution()
+    result = solution.find_max_index(numbers)
+    print(f"output = {result}")
+    
+except ValueError:
+    print("Please enter a valid list of integers.") 
+        

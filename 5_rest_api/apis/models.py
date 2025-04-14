1c8596c54dbc0fd9b3f3b026ccff4fcd41b17049
ployMatsuri <ployvimonsirithammada@gmail.com>
2025-04-14 15:05:16 +0700

    create all models

--- 5_rest_api/apis/models.py
+++ 5_rest_api/apis/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+GENDER_CHOICES = [
+    ('หญิง','หญิง'),
+    ('ชาย','ชาย'),
+]
+
+class School(models.Model):
+    sch_id = models.AutoField(primary_key=True)
+    sch_name = models.CharField(max_length=255)
+    sch_code_name = models.CharField(max_length=255)
+    adress = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Classroom(models.Model):
+    class_id = models.AutoField(primary_key=True)
+    sch_id = models.ForeignKey(School,on_delete=models.CASCADE)
+    grade = models.IntegerField()
+    room = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Teacher(models.Model):
+    teacher_id = models.AutoField(primary_key=True)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    gender = models.CharField(max_length=25,choices=GENDER_CHOICES)
+
+class Student(models.Model):
+    student_id = models.AutoField(primary_key=True)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    gender = models.CharField(max_length=25,choices=GENDER_CHOICES)
+
+class TeacherClassroom(models.Model):
+    id = models.AutoField(primary_key=True)
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)

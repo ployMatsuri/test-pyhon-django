559107a87c63e55d02c29f5c0f82f7579f1592d0
ployMatsuri <ployvimonsirithammada@gmail.com>
2025-04-15 14:25:45 +0700

    register models to admin

--- 5_rest_api/apis/admin.py
+++ 5_rest_api/apis/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from apis.models import School, Classroom, Teacher, Student, TeacherClassroom
 
-# Register your models here.
+@admin.register(School)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['sch_id', 'sch_name', 'sch_code_name', 'address']
+    search_fields = ['sch_name', 'address']
+
+@admin.register(Classroom)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['class_id', 'grade', 'room', 'sch_id']
+    search_fields = ['class_id']
+    list_filter = ['grade', 'sch_id']
+
+@admin.register(Teacher)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['teacher_id', 'firstname', 'lastname', 'gender']
+    search_fields = ['firstname', 'lastname']
+    list_filter = ['gender']
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['student_id', 'firstname', 'lastname', 'gender', 'class_id']
+    search_fields = ['firstname', 'lastname']
+    list_filter = ['gender', 'class_id']
+
+@admin.register(TeacherClassroom)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'classroom', 'teacher']
+    search_fields = ['classroom', 'teacher']
+    list_filter = ['classroom', 'teacher']
